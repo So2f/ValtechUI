1bd816f25f356f35ca9b6d1a7dfcac520103cd65
So2f <soffiane.elkarouni@gmail.com>
2024-08-26 02:50:10 +0200

    adding the Card Section Component ft styles might need to fix some grid ui responsive later

---
+++ src/components/CardSection/CardSection.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Link from "next/link";
+import { useContent } from "../../context/IndexContext";
+import styles from "./cardSection.module.css";
+
+const CardSection = () => {
+  const { content } = useContent();
+
+  const cardGridContent = content.find(
+    (section) => section.type === "CARD_GRID"
+  );
+
+  if (!cardGridContent) return null;
+
+  return (
+    <section className={styles.cardSection}>
+      <div>
+        <h2 className={styles.sectionTitle}>{cardGridContent.title}</h2>
+
+        <div className={styles.cardGrid}>
+          {cardGridContent.cards?.map((card, index) => {
+            const isLargeCard = index === 0 || index === 5;
+
+            return (
+              <div
+                key={index}
+                className={`${styles.card} ${
+                  isLargeCard ? styles.largeCard : styles.smallCard
+                }`}
+                style={{
+                  backgroundImage: `url(https://placehold.co/${
+                    isLargeCard ? 588 : 284
+                  }x479/D1D3CA/D1D3CA)`,
+                }}
+              >
+                <div className={styles.cardContent}>
+                  <div className={styles.titleSubtitle}>
+                    <p style={{ marginBottom: "20px" }}>{card.subtitle}</p>
+                    <p>{card.title}</p>
+                  </div>
+
+                  <div className={styles.descriptionContainer}>
+                    <p className={styles.cardDescription}>{card.description}</p>
+                  </div>
+
+                  <div className={styles.buttonContainer}>
+                    <Link href="/details" className={styles.button}>
+                      Explore More
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CardSection;
