6b37cc560f1aeef747c8a30b76f99547be01fade
So2f <soffiane.elkarouni@gmail.com>
2024-08-26 02:45:21 +0200

    adding the Card Section Component ft styles might need to fix some grid ui responsive later

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@
 import Banner from "@/components/Banner/Banner";
 import styles from "./page.module.css";
 import ContentProviderWrapper from "./ContentProviderWrapper";
+import CardSection from "@/components/CardSection/CardSection";
 
 export default function Home() {
   return (
     <ContentProviderWrapper>
       <main className={styles.main}>
         <Banner />
+
+        <CardSection />
       </main>
     </ContentProviderWrapper>
   );
